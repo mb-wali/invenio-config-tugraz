108e2fe8b1b1ebdd26028df1bd06dc64c8033442
mb <mb_wali@hotmail.com>
2021-01-26 18:57:58 +0100

    feature: draft_doi

--- invenio_config_tugraz/doi_mapping.py
+++ invenio_config_tugraz/doi_mapping.py
@@ -10,12 +10,26 @@
 import json
 import os
 from datacite import DataCiteRESTClient, schema43
+from flask import current_app
+from jsonschema import ValidationError
 
-
-class DoiMinting():
+class DoiMinting:
     """DOI minting invenioRDM records."""
+    
+    def validate(self, metadata):
+        """Validate the json object."""
 
-    def map_doi(metadata):
+        if schema43.validate(metadata):
+            return True
+
+        try:
+            schema43.validator.validate(metadata)
+        except ValidationError as e:
+            print(e.args[0])
+
+        return False
+
+    def map_doi(self, metadata):
         """Mapping the metadata."""
         print(metadata['resource_type']['type'])
 
@@ -42,23 +56,32 @@ class DoiMinting():
         'schemaVersion': 'http://datacite.org/schema/kernel-4',
         }
 
-        # validate the json
-        isvalid = valid(data)
-        
+        # validate json
+        self.validate(data)
+        print(self.validate(data))
+
         return data
 
-    def validate(metadata):
-        """Validate the json object."""
+    def get_config(self):
+        """Get the configuration variables from the environment."""
+        username = current_app.config['INVENIO_CONFIG_DATACITE_API_USERNAME']
+        password = current_app.config['INVENIO_CONFIG_DATACITE_API_PASSWORD']
+        prod_prefix = current_app.config['INVENIO_CONFIG_DATACITE_PREFIX']
+        test_prefix = current_app.config['INVENIO_CONFIG_DATACITE_TEST_PREFIX']
 
-        if schema43.validate(metadata):
-            return True
+        return (username, password, prod_prefix, test_prefix)
 
-        try:
-            schema43.validator.validate(metadata)
-        except ValidationError as e:
-            print(e.args[0])
+    def post_draft_doi(self, metadata):
+        """Post draft."""
+        
+        # get configs
+        username, password, prod_prefix, test_prefix = self.get_config()
 
-        return False
+        d = DataCiteRESTClient(
+            username=username,
+            password=password,
+            test_mode="test_mode",
+            prefix=test_prefix,
+            )
 
-    def post_draft_doi():
-        return "posting here."
+        return d.draft_doi(metadata=metadata)
